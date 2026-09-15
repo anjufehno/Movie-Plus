bbc346d872138b0ee228938136da11b1d90b4c2a
Jelizaveta Kruglova <114922000+anjufehno@users.noreply.github.com>
2026-09-15 09:49:40 +0300

    Add static image TypeScript declarations

---
+++ types/assets.d.ts
@@ -0,0 +1,14 @@
+declare module "*.jpg" {
+  const image: import("next/image").StaticImageData;
+  export default image;
+}
+
+declare module "*.jpeg" {
+  const image: import("next/image").StaticImageData;
+  export default image;
+}
+
+declare module "*.png" {
+  const image: import("next/image").StaticImageData;
+  export default image;
+}
